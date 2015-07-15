02d872f5271b751d44f20c13b0931ade41920af5
Szabolcs Ivan <szabolcs.ivan@gmail.com>
2015-07-15 10:22:23 +0200

    documenting, toSatString+toString

--- src/main/scala/com/mind_era/zizized/util/util.scala
+++ src/main/scala/com/mind_era/zizized/util/util.scala
@@ -5,13 +5,39 @@
 package com.mind_era.zizized.util
 
 package object util {
+  // scala's Vector is at least as good as the one in "util/vector.h"
+  // TODO this way it's util.util.Vector which is ugly a bit
   type Vector[T] = scala.`package`.Vector[T]
 }
 
+/**
+ * util/lbool.h and util/lbool.cpp : ternary Boolean value
+ * operator<< is now toString()
+ * to_sat_str( LBool* ) is now a member toSatString() of trait LBool
+ * there are three case objects
+ * LBool( true ) and LBool( false ) returns the case objects TRUE and FALSE respectively
+ * unary negation ~ maps TRUE to FALSE and FALSE to TRUE, leaves UNDEF unchanged
+ */ 
 sealed trait LBool {
+  /**
+   * ~TRUE = FALSE, ~FALSE = TRUE, ~UNDEF = UNDEF
+   */
   def unary_~ : LBool = if (this == TRUE) FALSE else if ( this == FALSE )  TRUE else UNDEF
+  /**
+   * LBool( true ) returns TRUE, LBool( false ) returns FALSE
+   */
   def apply( b : Boolean ) : LBool = if ( b ) TRUE else FALSE
+  def toSatString : String
 }
-case object TRUE extends LBool
-case object FALSE extends LBool
-case object UNDEF extends LBool
+case object TRUE extends LBool {
+  override def toString() : String = "l_true"
+  val toSatString : String = "satisfiable"
+}
+case object FALSE extends LBool {
+  override def toString() : String = "l_false"
+  val toSatString : String = "unsatisfiable"
+}
+case object UNDEF extends LBool {
+  override def toString() : String = "l_undef"
+  val toSatString : String = "unknonwn"
+}
