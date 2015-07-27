5f1f6350fe2549d63baac1278566ae5bb00b2b7b
Szabolcs Ivan <szabolcs.ivan@gmail.com>
2015-07-27 12:55:35 +0200

    stat classes used by satsolver, initial commmit

---
+++ src/main/scala/com/mind_era/zizized/sat/Stats.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015 Mind Eratosthenes Kft.
+ * License: AGPL v3
+ */
+package com.mind_era.zizized.sat
+
+import spire.math.UInt
+import com.mind_era.zizized.util.Statistics
+
+/**
+ * Statistic counters for the SAT solver.
+ * 
+ * @author Szabolcs Ivan
+ * @since 1.0
+ */
+final case class Stats(
+    var mkVar : UInt = UInt(0),
+    var mkBinClause : UInt = UInt(0),
+    var mkTerClause : UInt = UInt(0),
+    var conflict : UInt = UInt(0),
+    var propagate : UInt = UInt(0),
+    var terPropagate : UInt = UInt(0),
+    var decision : UInt = UInt(0),
+    var restart : UInt = UInt(0),
+    var gcClause : UInt = UInt(0),
+    var delClause : UInt = UInt(0),
+    var minimizedLits : UInt = UInt(0),
+    var dynSubRes : UInt = UInt(0)
+) {
+  def reset() : Unit = {
+    mkVar = UInt(0)
+    mkBinClause = UInt(0)
+    mkTerClause = UInt(0)
+    conflict = UInt(0)
+    propagate = UInt(0)
+    terPropagate = UInt(0)
+    decision = UInt(0)
+    restart = UInt(0)
+    gcClause = UInt(0)
+    delClause = UInt(0)
+    minimizedLits = UInt(0)
+    dynSubRes = UInt(0)
+  }
+  def collectStatistics( st : Statistics ) : Unit = {}
+}
---
+++ src/main/scala/com/mind_era/zizized/util/Statistics.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2015 Mind Eratosthenes Kft.
+ * License: AGPL v3
+ */
+package com.mind_era.zizized.util
+
+import scala.collection.mutable.HashMap
+import spire.math._
+import spire.algebra.Semigroup
+
+
+/**
+ * TODO document package com.mind_era.zizized.sat.Statistics
+ * 
+ * @author Szabolcs Ivan 
+ * @since version
+ */
+class Statistics extends HashMap[ String, Object ]{
+  //def update( key : String , inc : UInt ) : Unit = if( inc != UInt(0) ) insert( key, Number(inc) )  
+}
